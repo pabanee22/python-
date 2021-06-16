a9631e1765fb0b9b9311b7eef0c1a9fb9090abc7
pabanee22 <85046790+pabanee22@users.noreply.github.com>
2021-06-17 00:22:19 +0530

    Add files via upload

---
+++ numpy.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+get_ipython().system('pip install numpy')
+import numpy as np
+arr = np.array((1, 2, 3, 4, 5))
+arr
+
+
+# In[11]:
+
+
+arr = np.array([[3,2,1], [4,5,6]])
+arr
+
+
+# In[13]:
+
+
+arr.shape
+
+
+# In[8]:
+
+
+np.cos(arr)
+
+
+# In[1]:
+
+
+np.log(arr)
+
+
+# In[2]:
+
+
+arr_1 = np.array([1, 2, 3, 4, 5])
+arr_2 = np.array([6, 7, 8, 9, 4])
+
+
+# In[7]:
+
+
+np.zeros((1,2))
+
+
+# In[ ]:
+
+
+
+
